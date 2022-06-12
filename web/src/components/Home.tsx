922fff1335186e91fe8c7846222cb67de0eaa661
danjhkim <danjhkim@gmail.com>
2022-06-12 14:02:15 -0400

    added basic fetch for json data with env for development

--- web/src/components/Home.tsx
+++ web/src/components/Home.tsx
@@ -1,6 +1,22 @@
-import React, { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { fetchJson } from '../lib/api';
+const { REACT_APP_CMS_URL } = process.env;
 
 const HOME: FC<unknown> = () => {
+  console.log(REACT_APP_CMS_URL);
+  const [info, setInfo] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await fetchJson(`${REACT_APP_CMS_URL}/repos`);
+        setInfo(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
   return <div>Home</div>;
 };
 
